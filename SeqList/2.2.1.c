f052c25d6b0e55612e14b406cc36925bb352d60e
xun <examonlinepro@163.com>
2021-03-23 16:34:19 +0800

    第6，7题

--- SeqList/2.2.1.c
+++ SeqList/2.2.1.c
@@ -6,6 +6,7 @@ typedef struct sqlList{
     ElemType data[MaxSize];
     int length;
 }SqlList;
+bool Merge(SqlList lista,SqlList listb,SqlList *list);//P17D7合并两个有序表为一个新的有序表
 bool DeleteRep(SqlList *list);//P17D6删除重复元素
 bool Delete_From_s_to_t(SqlList *list,ElemType s,ElemType t);//P17D4D5删除值在s和t之间的元素
 bool DeleteX(SqlList *list,ElemType e);//P17D3删除顺序表中值为x的元素
@@ -19,22 +20,34 @@ ElemType GetElem(SqlList *list,int i);
 int LocateElem(SqlList *list,ElemType e);//按值查找元素，并返回元素的下标
 int main(){
     int i;
-    SqlList list;
+    SqlList list,lista,listb;
+    InitList(&lista);
+    InitList(&listb);
     InitList(&list);
-    int loac,num,ret,s,t;
-    while(loac!=-1){
-        scanf("%d%d",&loac,&num);
-        ListInsert(&list,loac,num);
+    printf("给a赋值\n");
+    int loc=0,num;
+    while(loc!=-1){
+        scanf("%d %d",&loc,&num);
+        ListInsert(&lista,loc,num);
     }
-    printf("输入的顺序表为\n");
-    for(i=1;i<=list.length;i++){
-        printf("%d     ",GetElem(&list,i));
+    printf("给b赋值\n");
+    loc=0;
+    while(loc!=-1){
+        scanf("%d %d",&loc,&num);
+        ListInsert(&listb,loc,num);
     }
-    printf("输入s和t的值\n");
-    scanf("%d%d",&s,&t);
-    Delete_From_s_to_t(&list,s,t);
+    printf("a的值为\n");
+    for(i=1;i<=lista.length;i++){
+        printf("%d  ",GetElem(&lista,i));
+    }
+    printf("\nb的值为\n");
+    for(i=1;i<=listb.length;i++){
+        printf("%d  ",GetElem(&listb,i));
+    }
+    Merge(lista,listb,&list);
+    printf("\nlist的值为\n");
     for(i=1;i<=list.length;i++){
-        printf("%d   ",GetElem(&list,i));
+        printf("%d  ",GetElem(&list,i));
     }
     return 0;
 }
@@ -84,7 +97,7 @@ int LocateElem(SqlList *list,ElemType e){
 }
 bool Empty(SqlList *list){
     if(list->length==0){
-        printf("顺序表为空\n");
+        printf("\n顺序表为空\n");
         return false;
     }
 }
@@ -142,6 +155,38 @@ bool Delete_From_s_to_t(SqlList *list,ElemType s,ElemType t){
     list->length=count;
     return true;
 }
-bool DeleteRep(SqlList *list){
-    
+bool DeleteRep(SqlList *list){  
+   if(list->length==0)
+    return false;
+    int i,j;
+    for(i=0,j=1;j<list->length;j++){
+        if(list->data[i]!=list->data[j]){
+            list->data[++i]=list->data[j];
+        }
+    }
+    list->length=i+1;
+    return true;
+}
+bool Merge(SqlList lista,SqlList listb,SqlList *list){//这一题尚未完成
+    if(lista.length+listb.length>MaxSize)
+        return false;
+    int i,j,k;
+    i=j=k=0;
+    while(i<lista.length&&j<listb.length){
+        if(lista.data[i]<listb.data[j]){
+            list->data[k++]=lista.data[i++];
+        }
+        else{
+            list->data[k++]=listb.data[j++];
+        }
+    }
+    while(i<lista.length){
+        list->data[k++]=lista.data[i++];
+    }
+    while (j<listb.length)
+    {
+        list->data[k++]=listb.data[j++];
+    }
+    list->length=k;
+    return false;
 }
