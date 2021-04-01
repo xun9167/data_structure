d51f251d440de132a36316e1da8f7b997c32e8e6
xun <examonlinepro@163.com>
2021-04-01 17:31:25 +0800

    双链表结束

--- SeqList/DLinkList_WithHead.c
+++ SeqList/DLinkList_WithHead.c
@@ -12,9 +12,18 @@ bool InitDLnikList(DNodeList *list);//初始化双链表
 bool Empty(DNodeList list);//判断带头节点的双链表是否为空
 void CreateDList(DNodeList *list);//尾插法建立带头节点的单链表
 void OutPrint(DNodeList list);//输出
+DNode *GetElem(DNodeList list,int loc);//对应下标元素
+bool InsertNextDNode(DNode *dnode,DNode *p);//在dnode元素后添加元素
+bool DeleteNextDNode(DNode *dnode);//删除指定节点
 int main(){
     DNodeList list;
     CreateDList(&list);
+    OutPrint(list);
+    int loc;
+    printf("输入值(位置)\n");
+    scanf("%d",&loc);
+    DNode *dnode=GetElem(list,loc);
+    DeleteNextDNode(dnode);
     OutPrint(list);
     return 0;
 }
@@ -58,12 +67,55 @@ void OutPrint(DNodeList list){
     p=list;
     p=p->next;
     printf("链表的值为：\n");
-   /* printf("%d\n",p->data);
-    printf("%d\n ",p->next->data);
-    printf("%d\n",p->next->next->data);*/
     while(p!=NULL){
-        printf("%d",p->data);
+        printf("%d     ",p->data);
         p=p->next;
     }
     printf("\n");
+}
+DNode *GetElem(DNodeList list,int loc){
+    if(loc<1){
+        printf("输入i值不合法（太小了）\n");
+        return NULL;
+    }
+    DNode *dnode;
+    dnode=list->next;
+    int j=1;
+    while(dnode!=NULL&&j<loc){
+        dnode=dnode->next;
+        j++;
+    }
+    if(dnode==NULL){
+        printf("输入i值不合法（太大了）\n");
+        return NULL;
+    }
+    return dnode;
+}
+bool InsertNextDNode(DNode *dnode,DNode *p){
+    if(dnode==NULL&&p==NULL){
+        printf("传入节点不合法\n");
+        return false;
+    }
+    
+    if(dnode->next!=NULL)//判断dnode是否为最后一个节点，最后一个节点需要特殊处理
+        dnode->next->prior=p;
+    p->prior=dnode;
+    p->next=dnode->next;
+    dnode->next=p;
+    return true;
+}
+bool DeleteNextDNode(DNode *dnode){
+    if(dnode==NULL){
+        printf("给定节点为空\n");
+        return false;
+    }
+    if(dnode->next==NULL){
+        printf("p为最后一个元素\n");
+        return false;
+    }
+    DNode *p=dnode->next;
+    p->prior=dnode->prior;
+    dnode->prior->next=p;
+    free(dnode);
+    return true;
 }
